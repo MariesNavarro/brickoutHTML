67b129d0b3df8c5ccd8903eb2befd6a4593057b9
Javier Corona Moreno <jackshel_10@hotmail.com>
2018-08-24 15:07:10 -0500

    Cambios Compartir

--- js/frontEnd.js
+++ js/frontEnd.js
@@ -517,6 +517,7 @@ function generaCeldas(){
 function checkPais(c){
   if(c === "NO"){
     nivelInterno = "no";
+    disclaimerIndex.setAttribute("class","flexDisplay");
     messagesGame.setAttribute("class", "flexDisplay");
     disclaimer.style.display = "none";
     paisnovalido.setAttribute("class", "flexDisplay");
@@ -525,6 +526,7 @@ function checkPais(c){
 function showPopUp(c){
   if(c === "NO"){
     nivelInterno = "no";
+    disclaimerIndex.setAttribute("class","flexDisplay");
     messagesGame.setAttribute("class", "flexDisplay");
     disclaimer.style.display = "none";
     emergentes.setAttribute("class", "flexDisplay");
